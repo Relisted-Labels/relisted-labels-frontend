0dbec8efe6bdf37c8a50abcd1bc546755212c5c0
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-09-13 04:56:30 +0100

    initial list hero

---
+++ src/components/listingsPage/ListHero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './ListHero.module.css'
+
+import React from 'react'
+
+const ListHero = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListHero
