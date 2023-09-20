1575ba508a9625f34c11ad8240cca5739b3e7a9c
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-09-20 09:40:04 +0100

    correct

--- src/components/itemViewing/ItemViewing.jsx
+++ src/components/itemViewing/ItemViewing.jsx
@@ -5,6 +5,27 @@ const ItemViewing = () => {
   return (
     <div className={Styles.container}>
       <img src={ItemImg}/>
+
+      <div className={Styles.description}>
+        <div className={Styles.price}>
+            <h2>
+                Curve Blazer
+            </h2>
+
+            <p>
+                #9,000
+            </p>
+        </div>
+
+        <div className={Styles.rating}>
+            <img src="" alt="" />
+
+            <div className={Styles.text}>
+                <h3>Mrs Bridget</h3>
+                <p></p>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
