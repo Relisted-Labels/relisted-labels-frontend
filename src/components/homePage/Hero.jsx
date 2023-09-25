9da5bcf6b9117569a64077035b37365add6c16bf
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-09-25 11:49:46 +0100

    landing page

--- src/components/homePage/Hero.jsx
+++ src/components/homePage/Hero.jsx
@@ -1,41 +1,38 @@
-import React from 'react'
-import HomePage from '../../assets/homepage-1.svg'
-// import Image1 from '../../assets/image-1.svg'
-// import Image2 from '../../assets/image-2.svg'
-// import Image3 from '../../assets/image-3.svg'
-import Styles from "./Hero.module.css"
-import Button from '../reusable/Button'
-import { useNavigate } from 'react-router-dom'
-
+import React from "react";
+import HomePage from "../../assets/web-homepage.png";
+import Styles from "./Hero.module.css";
+import Button from "../reusable/Button";
+import { useNavigate } from "react-router-dom";
 
 export const Hero = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-
-    const navigateToAuth = () => {
-        navigate("/auth")
-    };
+  const navigateToAuth = () => {
+    navigate("/auth");
+  };
   return (
     <>
-    <div className={Styles.home}>
-
-    <div className={Styles.navbar}>
-        <h3>RelistedLabels</h3>
-    </div>
-        <div>
-            <h2>
-                Join The World's Largest Shared Wardrobe
-            </h2>
+      <div className={Styles.home}>
+        <div className={Styles.container}>
+          <div className={Styles.navbar}>
+            <h3>RelistedLabels</h3>
+          </div>
+          <div>
+            <h2>Join The World's Largest Shared Wardrobe</h2>
             <p>Buy, Sell and Rent Your Favourite Cloths</p>
 
-            <Button name='Get Started' importance='primary' onClick={navigateToAuth} />
-        </div>
+            <Button
+              name="Get Started"
+              importance="primary"
+              onClick={navigateToAuth}
+            />
+          </div>
 
-        <img src={HomePage} alt="" />
-        
-    </div>
+          <img src={HomePage} alt="" />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Hero;
--- src/components/homePage/feature.jsx
+++ src/components/homePage/feature.jsx
@@ -1,7 +1,5 @@
 import Styles from './feature.module.css'
 
-import React from 'react'
-
 const feature = () => {
   return (
     <div className={Styles.container}>
