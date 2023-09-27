f00cddb7ce1b4adec473d6d81703d4a2cabe19c1
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-09-27 11:49:10 +0100

    add a loading state

--- src/components/authPage/ForgotPassword.jsx
+++ src/components/authPage/ForgotPassword.jsx
@@ -3,11 +3,15 @@ import { React, useState } from "react"
 import { Link } from "react-router-dom";
 import styles from "./AuthPage.module.css";
 import Button from "../reusable/Button";
+import Spinner from "../reusable/Spinner";
+
 import { MdArrowBackIos } from "react-icons/md"
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+     const [loading, setLoading] = useState(false);
+
   // const navigate = useNavigate();
 
 
@@ -17,38 +21,36 @@ const ForgotPassword = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
 
-    // Send a request to the forgot password route with the 'email' state.
     try {
-      const response = await fetch(
-        "https://relisted-labels-dev.onrender.com/auth/forgot-password",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email }),
+      // Simulate a loading delay of 2 seconds
+      setTimeout(async () => {
+        const response = await fetch(
+          "https://relisted-labels-dev.onrender.com/auth/forgot-password",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email }),
+          }
+        );
+
+        if (response.status === 200) {
+          setMessage("Password reset email sent. Check your inbox.");
+          console.log(response.data);
+        } else {
+          const data = await response.json();
+          setMessage(data.error || "Password reset request failed.");
+          console.log(response.data);
         }
-      );
-
-      if (response.status === 200) {
-        // Password reset email sent successfully
-        // const responseData = await response.json();
-        // const token = responseData.token;
-        setMessage("Password reset email sent. Check your inbox.");
-        console.log(response.data);
-
-
-        // Redirect to the frontend with the token
-        // window.location.href = `https://relisted-labels-frontend.vercel.app/resetpassword/${token}`;
-      } else {
-        const data = await response.json();
-        setMessage(data.error || "Password reset request failed.");
-        console.log(response.data);
-      }
+      }, 2000); 
     } catch (error) {
       console.error("An error occurred:", error);
       setMessage("An error occurred while processing your request.");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -58,7 +60,6 @@ return (
   <div className={styles.authContainer}>
     <div className={styles.authFormContainer}>
       <form className={styles.authForm}>
-
         <div className={styles.link}>
           <Link to="/auth" className={styles.linkTo}>
             <span className={styles.linkCont}>
@@ -81,7 +82,7 @@ return (
           <Button
             onClick={handleSubmit}
             importance="primary"
-            name="Confirm Email"
+            name={loading ? <Spinner /> : "Confirm Email"}
           />
         </span>
         {message && <div>{message}</div>}
