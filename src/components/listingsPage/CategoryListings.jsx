52ab418bff6c3c534f8f28f9a3ef6fde6aac11af
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-09-28 04:43:22 +0100

    ..

--- src/components/listingsPage/CategoryListings.jsx
+++ src/components/listingsPage/CategoryListings.jsx
@@ -1,17 +1,21 @@
 import { useState, useEffect } from "react";
 import Category from "./Category";
-import CategoryLink from "./CategoryLink";
+// import CategoryLink from "./CategoryLink";
 
 const CategoryListings = () => {
     return (
         <>
         {/* <h2>Categories</h2> */}
-        <CategoryLink />
-        <Category name="All categories"/>
+        {/* <CategoryLink /> */}
+        <Category name="Trending Items"/>
         
         {/* // <Category name="Trendy Casual" /> */}
-        <Category name="Women" />
-        <Category name="Traditional"/>
+        <Category name="Wedding Guest" />
+        <Category name="Holiday"/>
+        <Category name="Brunch"/>
+        <Category name="Dine with me"/>
+        <Category name="Aso-ebi"/>
+        <Category name="Lounge wear"/>
         </>
     )
 }
