f56b09cf4212115e02431ccad85b44cb8c8a8458
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-09-28 04:44:06 +0100

    create an array of cards and map it

--- src/components/listingsPage/Item.jsx
+++ src/components/listingsPage/Item.jsx
@@ -3,14 +3,36 @@ import styles from './Item.module.css'
 import Blazer from '../../assets/jumpsuit.png'
 
 const Item = () => {
+
+    const cards = [
+      {
+        productImg: Blazer,
+        text: "Dress, UK 12",
+        altText: "Dress, UK 12",
+        itemName: "Glamorous",
+        price: "Rent from E21",
+        discount: "PRP E90",
+      },
+     
+    ];
     
     return (
-        <div className={styles.item}>
-            <img src={Blazer} alt="Black woman on Blazer" className={styles.itemImage}/>
-            <h4 className={styles.itemName}>Office Blazer</h4>
-            <p>N8000</p>
-        </div>
-    )
+      <div>
+        {cards.map((card, index) => (
+          <div key={index}>
+            <img
+              src={card.productImg}
+              alt={card.altText}
+              className={styles.itemImage}
+            />
+            <h4 className={styles.itemName}>{card.itemName}</h4>
+            <h4 className={styles.text}>{card.text}</h4>
+            <h4 className={styles.itemName}>{card.price}</h4>
+            <h4 className={styles.discount}>{card.discount}</h4>
+          </div>
+        ))}
+      </div>
+    );
 }
 
 export default Item;
