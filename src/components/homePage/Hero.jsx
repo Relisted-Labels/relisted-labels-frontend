11566375ea96070220b9b1cefab7bc7487a4b874
learnwithalex <info@questgig.com>
2023-09-30 23:54:08 +0000

    Major Change

--- src/components/homePage/Hero.jsx
+++ src/components/homePage/Hero.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { useNavigate } from "react-router-dom";
+import  "./Hero.module.css";
 
 const navigation = [
-  { name: 'About', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Blog', href: '#' },
+  { name: 'New In', href: '#' },
+  { name: 'Designers', href: '#' },
+  { name: 'Clothing', href: '#' },
+  { name: 'Dresses', href: '#'},
+  { name: 'Bags', href: '#' },
+  { name: 'Accessories', href: '#' }
+
 ]
 
 
@@ -18,7 +23,7 @@ export default function Hero() {
   };
   return (
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
+      <header className="absolute inset-x-0 top-0 z-50 bg-gradient-to-r from-[#B0A3CE] to-[#ECCDCB] opacity-80 ">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5 ">
@@ -45,7 +50,7 @@ export default function Hero() {
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
+             Get Started <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </nav>
@@ -92,60 +97,28 @@ export default function Hero() {
           </Dialog.Panel>
         </Dialog>
       </header>
-
-      <div className="relative isolate px-6 pt-5 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ECCDCB] to-[#B0A3CE] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
+      
+      <div className="grid max-w-screen-xl mt-3 px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+        <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+            <span class="text-xs bg-gradient-to-r from-[#B0A3CE] to-[#ECCDCB] rounded-full text-gray-900 px-4 py-1.5 mr-3">Hot</span> <span class="text-sm font-medium">50% Discount for New Users</span> 
+            <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+        </a>
+          <h1 className="max-w-2xl mb-4 text-5xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Join the world’s largest shared wardrobe</h1>
+          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Rent, lend and sell over 80,000 designer fashion items</p>
+          <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-[#B0A3CE] hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+            Get started
+            <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+          </a>
+          <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            Continue as Guest
+          </a> 
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Read more about us in our blog.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Join The World's Largest Shared Wardrobe
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-            Buy, Sell and Rent Your Favourite Cloths
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                onClick={navigateToAuth}
-                className="rounded-md bg-[#B0A3CE] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#B0A3CE] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#B0A3CE]"
-              >
-                Get started
-              </a>
-            </div>
-          </div>
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#9e88d1] to-[#ffb6b1] opacity-40 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
-        </div>
+        <div className="hidden lg:mt-0  lg:col-span-5 lg:flex mt-3">
+          <img src="https://scontent.cdninstagram.com/v/t51.2885-15/301812806_106078615569021_7118600893979540292_n.jpg?stp=dst-jpg_e35&_nc_ht=scontent.cdninstagram.com&_nc_cat=109&_nc_ohc=Q6Za9j1OiW0AX8mRopj&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MjkxNDk2MDY1MDQzMTYwMzUwNQ%3D%3D.2-ccb7-5&oh=00_AfApMgLcmvMind4PLjc90yZxiGmdE6pg5KxSpM1P2FdApw&oe=651E5BF7&_nc_sid=10d13b" alt="mockup" className=''/>
+        </div>                
       </div>
-    </div>
+   
+      </div>
   )
 }
