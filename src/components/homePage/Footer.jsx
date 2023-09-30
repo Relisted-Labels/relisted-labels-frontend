9ddd86d31084ce9f7a2d66f5f7a38a1f4d52f0d9
learnwithalex <info@questgig.com>
2023-09-30 17:43:38 +0000

    Fixed Footer

--- src/components/homePage/Footer.jsx
+++ src/components/homePage/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <a href="#" class="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
          Relisted Labels  
       </a>
-      <p class="my-6 text-gray-500 dark:text-gray-400">Open-source library of over 400+ web components and interactive elements built for better web.</p>
+      <p class="my-6 text-gray-500 dark:text-gray-400">Buy, Sell and Rent Your Favourite Cloths</p>
       <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
           <li>
               <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
