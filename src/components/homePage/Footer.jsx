2d87a5c1ba8939187c7bec068dfb05447251493a
learnwithalex <info@questgig.com>
2023-10-02 10:11:10 +0000

    Fixed Bugs

--- src/components/homePage/Footer.jsx
+++ src/components/homePage/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 export default function Footer() {
   return (
-    <footer class="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
+    <footer class="p-4 bg-white md:p-8 lg:p-10">
   <div class="mx-auto max-w-screen-xl text-center">
-      <a href="#" class="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
+      <a href="#" class="flex justify-center items-center text-2xl font-semibold text-gray-900">
          Relisted Labels  
       </a>
-      <p class="my-6 text-gray-500 dark:text-gray-400">Buy, Sell and Rent Your Favourite Cloths</p>
-      <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+      <p class="my-6 text-gray-500 ">Buy, Sell and Rent Your Favourite Cloths</p>
+      <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900">
           <li>
               <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
           </li>
@@ -15,7 +15,7 @@ export default function Footer() {
               <a href="#" class="mr-4 hover:underline md:mr-6">Contact</a>
           </li>
       </ul>
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">Relisted Labels ™</a>. All Rights Reserved.</span>
+      <span class="text-sm text-gray-500 sm:text-center ">© 2023 <a href="#" class="hover:underline">Relisted Labels ™</a>. All Rights Reserved.</span>
   </div>
 </footer>
   )
--- src/components/homePage/SliderSecond.jsx
+++ src/components/homePage/SliderSecond.jsx
@@ -55,13 +55,13 @@ function HeroSection() {
 };
  
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-10 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-4">
-          <h1 className="max-w-2xl mb-4 text-1xl font-extrabold tracking-tight leading-none md:text-1xl xl:text-1xl dark:text-white">
+          <h1 className="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-2xl xl:text-1xl">
           New designers to know
           </h1>
-          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
 Get your Wish List ready as we present the new guard of brands mastering consciously crafted, modern luxury
           </p>
          
