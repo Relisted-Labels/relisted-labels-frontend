a8a7fbc6a13ab14d95cdf0cd34f99b71517437b4
learnwithalex <info@questgig.com>
2023-10-03 19:13:31 +0100

    Added Space

--- src/components/homePage/LastFeature.jsx
+++ src/components/homePage/LastFeature.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function LastFeature() {
     return (
 <div className="bg-white ">
-    <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+    <div className="gap-8 items-center mt-3 py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
         <img className="w-half relative z-10" data-aos="fade-down" data-aos-duration="1200" src="https://questgig.com/api/relisted/images/image10.jpg" alt="Relistlabels"/>
        
         <div className="mt-4 md:mt-0 relative z-10">
